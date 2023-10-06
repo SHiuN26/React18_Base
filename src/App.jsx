5b9a23b1a92c6ba22d193cee1de370c80cee9c3a
SHiuN26 <justodme26@gmail.com>
2023-10-06 15:26:25 +0800

    first branch

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <div className="flex justify-center items-center text-3xl font-bold underline bg-red-200 h-[100vh] w-full">
         React 18 + Vite + Tailwind + Antd V5
+        This is My First Branch
       </div>
 
     </>
