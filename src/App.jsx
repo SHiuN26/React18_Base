a733cd80b9c2da0b18a2b4e55e494103518a7a2a
SHiuN26 <justodme26@gmail.com>
2023-10-06 14:47:53 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center text-3xl font-bold underline bg-red-200 h-[100vh] w-full">
+        React 18 + Vite + Tailwind + Antd V5
+      </div>
+
+    </>
+  )
+}
+
+export default App
